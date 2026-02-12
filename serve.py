5b0ffee11b789154c1751e6e3af70b087a9b0fc2
Camyla Genelice <genelicecamyla@gmail.com>
2026-02-12 09:47:36 -0300

    feat: adição do CORS
    Agora o servidor recebe as requisições do frontend

--- serve.py
+++ serve.py
@@ -7,7 +7,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # Em produção, coloque o endereço do seu front-end
+    allow_origins=["http://localhost:3000"], # Em produção, coloque o endereço do seu front-end
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
